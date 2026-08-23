8a77d11d0a27e6fdba0b9e350c6a0e875c785d84
Mallikarjun Shankar <shankarmallikarjun@hotmail.com>
2026-08-23 15:49:07 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/23/2026, 3:49:07 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+};

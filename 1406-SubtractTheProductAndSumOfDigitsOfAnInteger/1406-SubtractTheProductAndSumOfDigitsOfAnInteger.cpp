cc318fb354141761941c12f3df1a2e2756b0e545
Mallikarjun Shankar <shankarmallikarjun@hotmail.com>
2026-08-25 10:32:59 +0530

    Create 1406-SubtractTheProductAndSumOfDigitsOfAnInteger

---
+++ 1406-SubtractTheProductAndSumOfDigitsOfAnInteger/1406-SubtractTheProductAndSumOfDigitsOfAnInteger.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/25/2026, 10:33:01 AM
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int prod = 1, sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            prod *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        return prod - sum;
+    }
+};

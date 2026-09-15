27db6ae4ff01aa57966914e39c7229f7d149f866
Mallikarjun Shankar <shankarmallikarjun@hotmail.com>
2026-09-15 15:24:06 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.cpp
@@ -0,0 +1,23 @@
+// Last updated: 9/15/2026, 3:24:06 PM
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0;
+        int high = 0;
+        for (char c : s) {
+            if (c == '(') {
+                low++;
+                high++;
+            } else if (c == ')') {
+                low--;
+                high--;
+            } else {
+                low--;
+                high++;
+            }
+            if (high < 0) return false;
+            low = max(low, 0);
+        }
+    return low == 0;    
+    }
+};
